7283ad389db6d319fd2d8890bf2c3253e7fcad07
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-03-28 12:13:53 +0100

    opencall animation

---
+++ src/app/[lang]/opencall-animation/page.tsx
@@ -0,0 +1,9 @@
+import OpenCallAni from "../../../components/opencall"
+
+export default async function Opencall() {
+  return (
+    <div className="opencall-ani">
+      <OpenCallAni/>
+    </div>
+  )
+}
