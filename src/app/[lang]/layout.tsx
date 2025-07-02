9c53620f47ddfbc3afd563af4b9adfb528bfe46f
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-07-02 11:57:03 +0200

    fix

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,6 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
-import './globals.scss';
+import '../globals.scss';
 
 export default function RootLayout({
   children,
