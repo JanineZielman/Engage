81ce2056e06cf435cf150952d688a5295a40c167
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-07-02 12:25:58 +0200

    update

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,19 +1,22 @@
-import { PrismicPreview } from "@prismicio/next";
-import { repositoryName } from "@/prismicio";
+export const dynamic = 'force-dynamic';
+
+import { PrismicPreview } from '@prismicio/next';
+import { repositoryName } from '@/prismicio';
 import '../globals.scss';
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export default async function LangLayout({
+  children
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+
+
   return (
-    <html lang="en">
-      <head>
-        <script defer data-domain="engagebiennial.nl" src="https://plausible.io/js/script.js"></script>
-      </head>
-      <body>{children}</body>
-      <PrismicPreview repositoryName={repositoryName} />
+    <html>
+      <body>
+        {children}
+        <PrismicPreview repositoryName={repositoryName} />
+      </body>
     </html>
   );
 }
