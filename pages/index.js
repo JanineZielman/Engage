9afa61a6ccfa13f1fec9d4efe22514748e3650da
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-08-01 09:35:01 +0200

    homepage

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,21 @@ import { createClient } from "../prismicio";
 import { Layout } from "../components/Layout";
 
 
-const Index = ({ home, navigation}) => {
-
+const Index = ({ home, navigation, about, team}) => {
   return (
     <div className="homepage">
       <Layout menu={navigation.results[0].data} page={home}>
         <SliceZone slices={home.data.slices} components={components} />
+        {about?.data.slices &&
+          <div id="about">
+            <SliceZone slices={about.data.slices} components={components} />
+          </div>
+        }
+        {team?.data.slices &&
+          <div id="team">
+            <SliceZone slices={team.data.slices} components={components} />
+          </div>
+        }
       </Layout>
     </div>
   );
@@ -22,13 +31,17 @@ export async function getStaticProps({ locale, previewData }) {
   const client = createClient({ previewData });
 
   const home = await client.getSingle("homepage", { lang: locale });
+  const about = await client.getByUID("page", "about", { lang: locale });
+  const team = await client.getByUID("page", "team", { lang: locale });
   const navigation = await client.getByType("navigation",  { lang: locale });
 
 
   return {
     props: {
      home,
-     navigation
+     navigation,
+     about,
+     team
     },
   };
 }
